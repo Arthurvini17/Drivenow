25cbb1cfe2b1e8da477185cc97d0e928176f6967
arthur <arthurdedsec@gmail.com>
2025-02-06 19:09:33 -0300

    att docker

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', [HomeController::class, 'index'])->name('home.index');
 
 Route::get('/counter', Counter::class);
-Route::get('/home/{id}',[HomeController::class, 'show'])->name('vehicles.show');
+Route::get('/vehicles/{id}',[HomeController::class, 'show'])->name('vehicles.show');
