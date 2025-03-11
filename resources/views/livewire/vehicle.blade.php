7d2e01f9dd638b8b70757f16f4b10aa50e87b9ef
arthur <arthurdedsec@gmail.com>
2025-03-10 23:29:09 -0300

    revertendo alteração na view home da image

--- resources/views/livewire/vehicle.blade.php
+++ resources/views/livewire/vehicle.blade.php
@@ -27,8 +27,7 @@
                 Preço: <span class="text-black">{{ number_format($vehicle->price, 2, ',', '.') }}</span></h1>
               <p class="text-fundoescuro">Ano lançamento: {{$vehicle->year}}</p>
             </div>
-            <img src="{{ Storage::url($vehicles->image) }}" class="w-30" alt="">
-
+            <img src="{{ asset('storage/' . $vehicle->image) }}" class="w-20 h-20 rounded-md">
           </div>
         </a>
       </div>
