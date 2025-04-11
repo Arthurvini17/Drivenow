5620e2b3ff16e87209d408446cbebc5e4d78ab0f
Arthur <arthurdedsec@gmail.com>
2025-04-11 00:14:31 -0300

    bucket image

--- app/Livewire/CreateVehicle.php
+++ app/Livewire/CreateVehicle.php
@@ -43,7 +43,7 @@ class CreateVehicle extends Component
     {
         //salvando a imagem na pasta public 
         if ($this->image) {
-            $imagePath = $this->image->store('images', 'public');
+            $imagePath = $this->image->store('images', 'r2');
 
             $validated['image'] = $imagePath;
         }
